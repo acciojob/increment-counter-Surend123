b4e9bdfd3aafbd8f3f6a4b68e6dda76e97880909
Surendra <sky958447@gmail.com>
2025-02-03 13:54:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let count=0;
+function countIncrement(){
+	alert(`${count}`);
+	count++;
+	let counter = document.getElementById("counter");
+	counter.textContent = `${count}`;
+}
