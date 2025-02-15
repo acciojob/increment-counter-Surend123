5dd0f3efa0b2b476e303f74e16e85c0b0e14cd39
Surendra <sky958447@gmail.com>
2025-02-15 12:30:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let count=0;
 let counter = document.getElementById("counter");
 let incrementBtn = document.getElementById("incrementBtn");
 incrementBtn.addEventListener('click',function(){
-	alert(`${count}`);
+	alert(count);
 	count++;
 	counter.innerText = count;
 });
